9c5edd75afbdbdfd7be4eabdd0723ab5198e0af8
phatzz12 <phatngo332@gmail.com>
2025-03-06 11:23:28 +0700

    lab01

---
+++ lab1/ex2_01.py
@@ -0,0 +1,3 @@
+ten  = input("nhap ten cua ban: ")
+tuoi = input ("nhap tuoi: " )
+print("chao mung,",ten,"!ban",tuoi,"tuoi.")
---
+++ lab1/ex3_01.py
@@ -0,0 +1,3 @@
+ban_kinh =float(input("nhap ban kinh hinh tron: "))
+dien_tich=3.14 *(ban_kinh **2)
+print("dein tich cua hinh tron:",dien_tich)
